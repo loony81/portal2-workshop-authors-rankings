e7fd164a709e80d40856dd9d6a81b42edd2ab88e
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-04-29 10:39:40 +0300

    remove country field from the Author model and instead add coop_maps; update cron.py to collect cooperative maps info; update index.html by replacing Country column with Coop maps column, add the ability to sort by coop maps

--- authors/cron.py
+++ authors/cron.py
@@ -14,6 +14,13 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 
+def make_request(url):
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen(req, context=ctx).read()
+    return BeautifulSoup(html, 'html.parser')
+
+
+
 def update_steamids():
     # use the SteamGroup.py module to fetch the SteamID64's of all the Steam group members and save them to steamids.txt
     members = steamgroup.get_steam_ids()
@@ -21,6 +28,7 @@ def update_steamids():
     for member in members:
         fhand.write(str(member) + '\n')
     fhand.close()
+
 
 
 def update_author_table():
@@ -32,11 +40,8 @@ def update_author_table():
     for steamid in fhand:
         # first of all find out how many followers and Portal 2 map submissions an author have
         try:
-            url = 'https://steamcommunity.com/profiles/' + steamid.replace('\n', '') + '/myworkshopfiles?appid=620'
-            req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # trick the server into thinking that the request is made from a genuine browser
-            html = urllib.request.urlopen(req, context=ctx).read()
-            soup = BeautifulSoup(html, 'html.parser')
-            followers = int(soup.find_all('div', class_='followStat')[0].text.replace(',', '')) # in case there is a comma remove it
+            soup = make_request('https://steamcommunity.com/profiles/' + steamid.replace('\n', '') + '/myworkshopfiles?appid=620')
+            followers = int(soup.find_all('div', class_='followStat')[0].text.replace(',', '')) # in case there is a comma to separate thousands, remove it
             submissions = soup.find_all('div', class_='workshopBrowsePagingInfo')
             if len(submissions) > 0:
                 # extract all numerical characters from text
@@ -47,21 +52,37 @@ def update_author_table():
         except:
             # if something goes wrong with the request just skip it by setting the followers variable to 0
             followers = 0
-        # if an author has at least 5 followers and 1 submission related to Portal 2, extract additional info using the Steam api and add them to the database
+        # if an author has at least 5 followers and 1 submission related to Portal 2, extract his personaname, avatar, profile_url and then make another request to find out how many coop maps he has and add him to the database
         if followers > 5 and submissions > 0:
-            create_author(steamid, followers, submissions)
+            personaname = soup.select_one('#HeaderUserInfoName a').text
+            profile_url = soup.select_one('#HeaderUserInfoName a')['href']
+            avatar = soup.select_one('.playerAvatar > img')['src'] # there could be another div with an image inside this div, so we need to grab only the image which is a direct child
+            # in case it's an animated gif or png image, just set it equal to default avatar
+            if avatar.endswith('.gif') or avatar.endswith('.png'):
+                    avatar = 'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/6f/6f982210fe9a377fdb74d9a836e4d670f0fd68fc.jpg'
+            else:
+                avatar = avatar.replace('_medium', '')
+            create_author(steamid, personaname, avatar, profile_url, followers, submissions)
 
 
-def create_author(steamid, followers, submissions):
+
+def create_author(steamid, personaname, avatar, profile_url, followers, submissions):
     try:
-        author = requests.get('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=0E167006C0EE4A1C3CD518D4944D47B1&steamids=' + steamid).json()['response']['players'][0]
+        soup = make_request('https://steamcommunity.com/profiles/' + steamid.replace('\n', '') + '/myworkshopfiles?appid=620&requiredtags%5B%5D=Cooperative')
+        coop_maps = soup.find_all('div', class_='workshopBrowsePagingInfo')
+        if len(coop_maps) > 0:
+            # extract all numerical characters from text
+            numerical_characters = re.findall('[0-9]+', coop_maps[0].text)
+            coop_maps = int(numerical_characters[-1])
+        else:
+            coop_maps = 0
         Author.objects.create(
-            nicname=author['personaname'],
-            avatar=author['avatar'],
-            profile_url=author['profileurl'],
-            loc_country_code=author.get('loccountrycode', '-'),
+            nicname=personaname,
+            avatar=avatar,
+            profile_url=profile_url,
             number_of_followers=followers,
-            workshop_submissions=submissions
+            workshop_submissions=submissions,
+            coop_maps=coop_maps
         )
     except:
         pass
--- authors/models.py
+++ authors/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
     nicname = models.CharField(max_length=255)
     profile_url = models.CharField(max_length=2083)
     avatar = models.CharField(max_length=2083)
-    loc_country_code = models.CharField(max_length=10, default='-')
     number_of_followers = models.IntegerField()
     workshop_submissions = models.IntegerField()
+    coop_maps = models.IntegerField()
 
