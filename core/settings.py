04cceb48df9ce9acee88bcfc00589f96a080cc4a
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-05 19:45:07 +0300

    set STATIC_ROOT in settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.42.0.1', '127.0.0.1', 'portal2-workshop-authors.herokuapp.com/']
+ALLOWED_HOSTS = ['10.42.0.1', '127.0.0.1', 'portal2-workshop-authors.herokuapp.com']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
