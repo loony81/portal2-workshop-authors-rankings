e6a8efb1c06f6a26bd2317b4f486415a9e5add80
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-06 20:19:04 +0300

    change the date inside scheduler.py

--- authors/authorsUpdater/scheduler.py
+++ authors/authorsUpdater/scheduler.py
@@ -6,6 +6,6 @@ from .authorsUpdateApi import update_authors_steamid_table, update_authors_autho
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='0', hour=21, minute=1, id='update_steamids_001', replace_existing=True)
-    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='0', hour=21, minute=5, replace_existing=True)
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='3', hour=21, minute=1, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='3', hour=21, minute=5, replace_existing=True)
     scheduler.start()
