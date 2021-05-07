2208740b1038d277a232b7f6a2ecd5e5afaa86da
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-08 00:29:03 +0300

    change BackgroundScheduler to BlockingScheduler to prevent main thread from exiting after 30 min

--- authors/authorsUpdater/scheduler.py
+++ authors/authorsUpdater/scheduler.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from .authorsUpdateApi import update_authors_steamid_table, update_authors_authortemp_table
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(update_authors_steamid_table, 'date', run_date=datetime(2021, 5, 7, 17, 52, 5), id='update_steamids_001', replace_existing=True)
-    scheduler.add_job(update_authors_authortemp_table, 'date', run_date=datetime(2021, 5, 7, 19, 19, 5), replace_existing=True)
+    scheduler = BlockingScheduler()
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='5', hour=3, minute=3, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='5', hour=3, minute=7, replace_existing=True)
     scheduler.start()
