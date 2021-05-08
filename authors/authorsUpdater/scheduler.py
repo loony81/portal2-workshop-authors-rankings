ef27665a171402b9a34ba4a3927ec5f0fc647cea
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-08 14:08:16 +0300

    update scheduler.py

--- authors/authorsUpdater/scheduler.py
+++ authors/authorsUpdater/scheduler.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from .authorsUpdateApi import update_authors_steamid_table, update_authors_authortemp_table
 
 
 def start():
-    scheduler = BlockingScheduler()
-    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='5', hour=3, minute=3, id='update_steamids_001', replace_existing=True)
-    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='5', hour=3, minute=7, replace_existing=True)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='5', hour=11, minute=10, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='5', hour=3, minute=15, replace_existing=True)
     scheduler.start()
