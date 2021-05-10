fcbc0346c7e9cc428ad9c1bdbe2e4a5d03fc680c
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-10 19:07:13 +0300

    update scheduler.py

--- authors/authorsUpdater/scheduler.py
+++ authors/authorsUpdater/scheduler.py
@@ -6,6 +6,6 @@ from .authorsUpdateApi import update_authors_steamid_table, update_authors_autho
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='0', hour=16, minute=5, id='update_steamids_001', replace_existing=True)
-    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='0', hour=16, minute=10, replace_existing=True)
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='1', hour=1, minute=5, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='1', hour=1, minute=10, replace_existing=True)
     scheduler.start()
