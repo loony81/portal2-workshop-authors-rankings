f02afea2d4157971b57dc047fc267fddbd806bd2
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-12 14:24:37 +0300

    add Steamid model to Admin panel

--- authors/admin.py
+++ authors/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, NoGroupAuthor, SteamGroupName, UpdateDate
+from .models import Author, NoGroupAuthor, SteamGroupName, UpdateDate, Steamid
 
 admin.site.register(Author)
 admin.site.register(NoGroupAuthor)
 admin.site.register(SteamGroupName)
 admin.site.register(UpdateDate)
+admin.site.register(Steamid)
