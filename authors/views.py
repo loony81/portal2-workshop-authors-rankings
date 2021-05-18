493d756c9c3f78370af9883ec3c9203837336a26
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-05-18 18:49:59 +0300

    find out the author's exact position; add an anchor link to scroll the viewport to the author's location on the webpage when the paged is loaded

--- authors/views.py
+++ authors/views.py
@@ -7,7 +7,7 @@ from .forms import AuthorNameForm
 
 def index(request):
     msg = ''
-    msg_class = ''
+    anchor = ''
 
     order_by = request.GET.get('order_by')
     # by default when extracting the authors from the db sort them by the number_of_followers column in descending order
@@ -29,7 +29,23 @@ def index(request):
                 # get him and find out his position in the ranking
                 author = Author.objects.get(nicname=search)
                 position = Author.objects.filter(number_of_followers__gt=author.number_of_followers).count()+1
-                page = int(position/items_per_page)+1
+                # there could be other authors with the same number of followers
+                same_number_of_followers = Author.objects.filter(number_of_followers=author.number_of_followers).count()
+                if same_number_of_followers > 1:
+                    # if there are, then get all of them and recalculate the position
+                    authors_with_the_same_number_of_followers = Author.objects.filter(number_of_followers=author.number_of_followers).order_by(order_by)
+                    for i in range(same_number_of_followers):
+                        if authors_with_the_same_number_of_followers[i].nicname == author.nicname:
+                            break
+                        position += 1
+                page = position / items_per_page
+                if page.is_integer():
+                    page = int(page)
+                else:
+                    page = int(page) + 1
+                print(position, page, author.nicname)
+                # anchor will allow the page to start at a certain point so that the author's position is inside the viewport
+                anchor = author.nicname
             else:
                 page = 1
                 msg = 'Sorry, no author by that name'
@@ -50,9 +66,9 @@ def index(request):
         'authors': authors,
         'order_by': order_by,
         'form': form,
-        'message': msg
+        'message': msg,
+        'anchor': anchor
     }
-
     return render(request, 'index.html', context)
 
 
