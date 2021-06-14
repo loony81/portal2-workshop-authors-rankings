47302c9444d24c3332dba693684b45391c5fd6f7
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-06-14 13:38:29 +0300

    add the ability to sort authors by name and set it as default

--- authors/views.py
+++ authors/views.py
@@ -9,38 +9,45 @@ from .forms import AuthorNameForm
 items_per_page = 20
 last_updated = UpdateDate.objects.get(model_name='Author')
 
+# to get the position when authors are ranked by the number of followers
+# def calculate_page(author):
+#     position = Author.objects.filter(number_of_followers__gt=author.number_of_followers).count() + 1
+#     # there could be other authors with the same number of followers
+#     same_number_of_followers = Author.objects.filter(number_of_followers=author.number_of_followers).count()
+#     if same_number_of_followers > 1:
+#         # if there are, then get all of them and recalculate the position
+#         authors_with_the_same_number_of_followers = Author.objects.filter(
+#             number_of_followers=author.number_of_followers).order_by('id')
+#         for i in range(same_number_of_followers):
+#             if authors_with_the_same_number_of_followers[i].nicname == author.nicname:
+#                 break
+#             position += 1
+#     page = position / items_per_page
+#     if page.is_integer():
+#         page = int(page)
+#     else:
+#         page = int(page) + 1
+#     return page
+
+# to get the position when authors are ranked by name
 def calculate_page(author):
-    position = Author.objects.filter(number_of_followers__gt=author.number_of_followers).count() + 1
-    # there could be other authors with the same number of followers
-    same_number_of_followers = Author.objects.filter(number_of_followers=author.number_of_followers).count()
-    if same_number_of_followers > 1:
-        # if there are, then get all of them and recalculate the position
-        authors_with_the_same_number_of_followers = Author.objects.filter(
-            number_of_followers=author.number_of_followers).order_by('id')
-        for i in range(same_number_of_followers):
-            if authors_with_the_same_number_of_followers[i].nicname == author.nicname:
-                break
-            position += 1
+    position = Author.objects.filter(nicname__lt=author.nicname).count() + 1
     page = position / items_per_page
     if page.is_integer():
         page = int(page)
     else:
         page = int(page) + 1
+    print(position)
     return page
-
 
 
 def index(request):
     msg = ''
     anchor = ''
-
     order_by = request.GET.get('order_by')
-    # by default when extracting the authors from the db sort them by the number_of_followers column in descending order
-    # and then by id for those authors who have the same number of followers
     if not order_by:
-        order_by = '-number_of_followers'
-    authors_list = Author.objects.order_by(order_by, 'id')
-    paginator = Paginator(authors_list, items_per_page)
+        order_by = 'nicname'
+
     # check if the form was submitted
     if request.GET.get('search'):
         form = AuthorNameForm(request.GET) # bind the form
@@ -70,6 +77,12 @@ def index(request):
 
     # reset the form
     form = AuthorNameForm()
+
+    if order_by == '-number_of_followers':
+        authors_list = Author.objects.order_by(order_by, 'id')
+    else:
+        authors_list = Author.objects.order_by(order_by, '-number_of_followers')
+    paginator = Paginator(authors_list, items_per_page)
 
     try:
         authors = paginator.page(page)
