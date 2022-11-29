3eee562fa8ecd352b9c17ceb8d84df5f81b790dc
Roman Nazarenko <r.nazarenko.81@gmail.com>
2022-11-29 17:34:01 +0200

    create superuser using custom manage.py command

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+echo $PG_PASSWORD
+python manage.py my_command --username admin --password $PG_PASSWORD --email loonyonline@gmail.com
