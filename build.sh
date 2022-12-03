a42c1771e9242bcdf0bec17697b02852ba6e0a97
Roman Nazarenko <r.nazarenko.81@gmail.com>
2022-12-03 19:20:19 +0200

    update build.sh to prevent creating another superuser

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-echo $PG_PASSWORD
-python manage.py my_command --username admin --password $PG_PASSWORD --email loonyonline@gmail.com
