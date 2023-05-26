02ac4d6752a37bff2df6b433c312c3496ca36775
Roman Nazarenko <r.nazarenko.81@gmail.com>
2023-05-26 11:25:22 +0300

    Change date from Friday to Saturday to run BackgroundScheduler, and also add another scheduler to run on Tuesdays

--- authors/authorsUpdater/scheduler.py
+++ authors/authorsUpdater/scheduler.py
@@ -6,6 +6,8 @@ from .authorsUpdateApi import update_authors_steamid_table, update_authors_autho
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='4', hour=3, minute=5, id='update_steamids_001', replace_existing=True)
-    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='4', hour=3, minute=10, replace_existing=True)
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='5', hour=3, minute=5, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='5', hour=3, minute=10, replace_existing=True)
+    scheduler.add_job(update_authors_steamid_table, 'cron', day_of_week='1', hour=3, minute=5, id='update_steamids_001', replace_existing=True)
+    scheduler.add_job(update_authors_authortemp_table, 'cron', day_of_week='1', hour=3, minute=10, replace_existing=True)
     scheduler.start()
